1a294f92f38c1a4458acc3b6b0c51a29fc5675d2
Mark Riley Bomberry <higgsofwolfram74@gmail.com>
2021-08-22 15:22:10 -0500

    breed filtering added

--- src/bot.py
+++ src/bot.py
@@ -14,20 +14,22 @@ bot = commands.Bot(command_prefix="~")
 class PetGetter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def hello(self, ctx):
-        await ctx.send("Howdy")
+        
 
     @commands.command(name="cat")
-    async def cat(self, ctx, *args):
+    async def cat(self, ctx, breed: str):
         cat = Pet_Pics(Path("./env-var/env.json"), "cat")
+        if len(breed) == 4:
+            cat.breed_response(breed)
+
         url = await cat.pet_get()
         await ctx.send(url)
 
     @commands.command(name="dog")
-    async def dog(self, ctx, *args):
+    async def dog(self, ctx, breed: int):
         dog = Pet_Pics(Path("./env-var/env.json"), "dog")
+        if breed > 0 and breed < 264:
+            dog.breed_response(str(breed))
         url = await dog.pet_get()
         await ctx.send(url)
 
@@ -44,6 +46,21 @@ class PetGetter(commands.Cog):
         dog.query_type(gif = True)
         url = await dog.pet_get()
         await ctx.send(url)
+
+    @commands.command(name="cat-breed")
+    async def cat_breed(self, ctx, breed: str):
+        cat_breeds = await Pet_Pics.get_breeds("cat")
+        breeds = {breed[1].lower(): breed[0] for breed in cat_breeds}
+        if breed in breeds:
+            await ctx.send(f"The breed {breed} has the id {breeds[breed]}.")
+
+    @commands.command(name="dog-breed")
+    async def dog_breed(self, ctx, breed: str):
+        cat_breeds = await Pet_Pics.get_breeds("dog")
+        breeds = {breed[1].lower(): breed[0] for breed in cat_breeds}
+        if breed.lower() in breeds:
+            await ctx.send(f"The breed {breed} has the id {breeds[breed]}.")
+
 
 bot.add_cog(PetGetter(bot))
 
--- src/catcaller.py
+++ src/catcaller.py
@@ -1,7 +1,7 @@
 import json
 import time
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple, Union
 import asyncio
 import aiohttp
 
@@ -9,7 +9,7 @@ import aiohttp
 #todo: Make sure user can't input malicious input for breed
 #^ make the responsibility belong to where breed is input
 class Pet_Pics():
-    query: str = ""
+    animal: str
     key: str
     link: str
     pictures: str
@@ -18,12 +18,16 @@ class Pet_Pics():
         self.path = path
 
         if pet == "cat":
-            self.link = "https://api.thecatapi.com/v1/images/search" + self.query
+            self.link = "https://api.thecatapi.com/v1/images/search"
             self.key_type = "CAT_API_KEY"
 
-        if pet == "dog":
-            self.link = "https://api.thedogapi.com/v1/images/search" + self.query
+
+        elif pet == "dog":
+            self.link = "https://api.thedogapi.com/v1/images/search"
             self.key_type = "DOG_API_KEY"
+
+        else:
+            print("Invalid animal type")
 
     async def get_key(self):
         self.key = json.load(self.path.open('r'))[self.key_type]
@@ -31,8 +35,7 @@ class Pet_Pics():
     def breed_response(self, breed:str):
         query = "?"
 
-        if len(breed) == 4:
-            self.link += query + "breed_ids=" + breed
+        self.link += query + "breed_ids=" + breed
 
     def query_type(self, gif: bool):
         query = "?"
@@ -48,7 +51,8 @@ class Pet_Pics():
         self.link += query
 
     async def load_pictures(self) -> Dict[str, str]:        
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers={'x-api-key': self.key}) as session:
+            print(f"Getting animal from {self.link}")
             async with session.get(self.link) as response:
                 object = await response.json()
 
@@ -65,6 +69,27 @@ class Pet_Pics():
 
         return self.pictures[0]['url']
 
+    async def get_breeds(animal: str) -> List[Tuple[Union[str,int], str]]:
+        if animal == 'cat':
+            ani = "https://api.thecatapi.com/v1/breeds"
+
+        elif animal == 'dog':
+            ani = "https://api.thedogapi.com/v1/breeds"
+        
+        else: 
+            print("Invalid animal")
+            return None
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(ani) as response:
+                object = await response.json()
+
+        time.sleep(1)
+
+        return [(breed['id'], breed['name']) for breed in object]
+
+
+
             
 if __name__ == "__main__":
     context = {
@@ -72,9 +97,12 @@ if __name__ == "__main__":
         "gif":0
     }
 
-    test = Pet_Pics(path=Path("./env-var/env.json"), pet="cat")
+    test = Pet_Pics(path=Path("./env-var/env.json"), pet="dog")
 
-    print(asyncio.run(test.simple_pet()))
+    breeds = asyncio.run(Pet_Pics.get_breeds("dog"))
+    time.sleep(1)
+    for breed in breeds:
+        print(f"The breed {breed[1]} has the id {breed[0]}")
     
     
     
